1fe702c401f942f55a30ed09c4cadeb58754d371
Edward Josh C. Hermano <ejoshcasmano@gmail.com>
2021-05-04 13:03:32 -0700

    Bumped up class authority levels. Started skeleton for code to look through keys and simple fill methods.

--- src/nonogramSolver/Nonogram.java
+++ src/nonogramSolver/Nonogram.java
@@ -1,22 +1,95 @@
 package nonogramSolver;
 
-import java.util.ArrayList;
-
 public class Nonogram
 {
-	public Nonogram(int width, int height, ArrayList<NonoKey> colKeysList, ArrayList<NonoKey> rowKeysList) {
-		grid = new Tile[height][width];
-		for(Tile[] row: grid) {
+	public Nonogram(int width, int height)
+	{
+		this.grid = new Tile[height][width];
+		for(Tile[] row: this.grid) {
 			for(Tile t: row) {
 				t = Tile.BLANK;
 			}
 		}
-		
-		this.colKeysList = colKeysList;
-		this.rowKeysList = rowKeysList;
+		completion = 0;
+	}
+	
+	public int getWidth() {
+		return this.grid[0].length;
+	}
+	
+	public int getHeight() {
+		return this.grid.length;
+	}
+	
+	public boolean isCompleted() {
+		return completion >= this.getWidth() * getHeight();
+	}
+	
+	public Tile getTile(int x, int y) {
+		return this.grid[y][x];
+	}
+	
+	public Tile[] getRow(int index) {
+		return grid[index];
+	}
+	
+	public Tile[] getCol(int index) {
+		int height = this.getHeight();
+		Tile[] col = new Tile[height];
+		for(int j = 0; j < height; j++)
+			col[j] = grid[j][index];
+		return col;
+	}
+	
+	public Tile[] getLine(int index, boolean isRow) {
+		if(isRow)
+			return this.getRow(index);
+		else
+			return this.getCol(index);
+	}
+	
+	/**
+	 * Replace all BLANK tiles in the indexed row/column with the specified tile type.
+	 * 
+	 * @param index The index of the row/column to pour.
+	 * @param tileType The type of Tile to pour into the row/column. Either Tile.FILLED or Tile.BLOCKED.
+	 * @param isRow true if pouring over a row, false if pouring over a column.
+	 */
+	public void pour(int index, Tile tileType, boolean isRow) {
+		int width = this.getWidth();
+		int height = this.getHeight();
+		if(isRow) {
+			for(int i = 0; i < width; i++) {
+				this.grid[index][i] = tileType;
+			}
+		}
+		else {
+			for(int j = 0; j < height; j++) {
+				this.grid[j][index] = tileType;
+			}
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		String solution = new String();
+		for(int j = 0; j < this.getHeight(); j++) {
+			for(int i = 0; i < this.getWidth(); i++) {
+				if(this.getTile(i, j)==Tile.FILL)
+					solution += "1 ";
+				else if(this.getTile(i, j)==Tile.SPACE)
+					solution += "0 ";
+				else if(this.getTile(i, j)==Tile.BLANK)
+					solution += "x ";
+				else
+					solution += "n ";					
+			}
+			solution += "\n";
+		}
+		return solution;
 	}
 	
 	private Tile[][] grid;
-	private ArrayList<NonoKey> colKeysList;
-	private ArrayList<NonoKey> rowKeysList;
+	private int completion;
 }
--- src/nonogramSolver/NonogramSolver.java
+++ src/nonogramSolver/NonogramSolver.java
@@ -2,63 +2,61 @@ package nonogramSolver;
 
 import java.util.ArrayList;
 
-//	00000
-//	01110
-//	11111
-//	10010
-//	01100
+/**
+ * TEMP: list of solution methods
+ * 
+ * Simple Fills:	Fill any tiles where all extremes will overlap.
+ * Simple Spaces:	Space any tiles where all extremes cannot reach.
+ * Forcing:	Space any tiles where a section cannot fit. (More specific Simple Spaces)
+ * Glue:	Filling tiles where pushing a section that starts near or against a block/wall will always overlap. (More specific simple fills)
+ * Joining and splitting:	Check to see whether a section can or cannot be placed given the current fills.
+ * Punctuate:		Enclose each section with a space
+ * 
+ * @author Edward Josh Hermano
+ */
 public class NonogramSolver
 {
-	private static final int[][] COL_KEYS =
-		{
-			{2},
-			{2,1},
-			{2,1},
-			{3},
-			{1},
-		};
-	
-	private static final int[][] ROW_KEYS =
-		{
-			{2},
-			{2,1},
-			{2,1},
-			{3},
-			{1},
-		};
-	
-	public static void main(String[] args) {
-		NonogramSolver solver = new NonogramSolver();
-		Nonogram test = new Nonogram(NonogramSolver.WIDTH,NonogramSolver.HEIGHT,solver.getColKeys(),solver.getRowKeys());
+	public NonogramSolver(ArrayList<NonoKey> colKeysList, ArrayList<NonoKey> rowKeysList) {
+		this.colKeysList = colKeysList;
+		this.rowKeysList = rowKeysList;
+		
+		this.nonogram = new Nonogram(colKeysList.size(), rowKeysList.size());
 	}
 	
-	/**
-	 * Gets the keys for the columns of the nonogram.
-	 * Keys are organized left-to-right, values in the keys are organized top-to-bottom.
-	 * 
-	 * @return
-	 */
-	private ArrayList<NonoKey> getColKeys() {
-		ArrayList<NonoKey> colKeyList = new ArrayList<>();
-		ArrayList<Integer> colA = new ArrayList<>(2);
-		ArrayList<Integer> colB = new ArrayList<>(2);
-		ArrayList<Integer> colC = new ArrayList<>(2);
-		ArrayList<Integer> colD = new ArrayList<>(2);
-		ArrayList<Integer> colE = new ArrayList<>(2);
-		return colKeyList;
+	
+	public String getSolutionAsString() {
+		this.solve();
+		int height = nonogram.getHeight();
+		int width = nonogram.getWidth();
+		
+		return nonogram.toString();
 	}
 	
-	/**
-	 * Gets the keys for the row of the nonogram.
-	 * Keys are organized top-to-bottom, values in the keys are organized left-to-right.
-	 * 
-	 * @return
-	 */
-	private ArrayList<NonoKey> getRowKeys() {
-		ArrayList<NonoKey> rowKeyList = new ArrayList<>();
-		return rowKeyList;
+	private void solve() {
+		int test = 0;
+		while(!nonogram.isCompleted() && test < 1) {
+			System.out.println("solve " + test);
+			this.processList(this.colKeysList, false);
+			this.processList(this.rowKeysList, true);
+			test++;
+		}
 	}
 	
-	private static final int WIDTH = 5;
-	private static final int HEIGHT = 5;
+	private void processList(ArrayList<NonoKey> keys, boolean isRow) {
+		for(int i = 0; i < keys.size(); i++) {
+			simpleFill(nonogram.getLine(i, isRow), keys.get(i));
+		}
+	}
+	
+	private void simpleFill(Tile[] tiles, NonoKey key) {
+		if(key.get(0)==0)
+			nonogram.pour(i, Tile.SPACE);
+		else if(keys.get(i).get(0)==nonogram.getWidth() && isRow ||
+				keys.get(i).get(0)==nonogram.getHeight() && !isRow)
+			nonogram.pour(i, Tile.FILL, isRow);
+	}
+	
+	private ArrayList<NonoKey> colKeysList;
+	private ArrayList<NonoKey> rowKeysList;
+	private Nonogram nonogram;
 }
